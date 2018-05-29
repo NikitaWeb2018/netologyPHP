c0673270f6f19b54c44ed9ee5440cd174c49f617
NikitaWeb2018 <38716672+NikitaWeb2018@users.noreply.github.com>
2018-05-29 18:00:10 +0400

    Update hw5(2).php

--- hw5(2).php
+++ hw5(2).php
@@ -1,9 +1,25 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: simonov
+ * Date: 24.05.2018
+ * Time: 2:15
+ */
 
+$link = 'http://api.openweathermap.org/data/2.5/forecast';
+$city = '499099';
+$appid = '7e6f586e16e9c5e03b6ac9187492deb4';
 
-$api='http://api.openweathermap.org/data/2.5/forecast?id=499099&appid=7e6f586e16e9c5e03b6ac9187492deb4';
+$api = "$link?id=$city&appid=$appid";
 $respond = file_get_contents($api);
+if ($respond === false) {
+    exit('Не удалось получить данные');
+}
 $respond = json_decode($respond);
+if ($respond === null) {
+    exit('Ошибка декодирования json');
+}
+
 $respond->list[0]->main->temp = $respond->list[0]->main->temp -273.15;
 
 
@@ -23,16 +39,40 @@ $respond->list[0]->main->temp = $respond->list[0]->main->temp -273.15;
         <td>Weather in <?php echo $respond->city->name; ?>:</td>
     </tr>
     <tr>
-        <td>Temperature: <?php echo $respond->list[0]->main->temp; ?> °C</td>
+        <td>
+            Temperature: <?php if (!empty($respond->list[0]->main->temp)) {
+                echo $respond->list[0]->main->temp .' °C';
+            } else {
+                echo 'Не удалось получить данные';
+            } ?>
+        </td>
     </tr>
     <tr>
-        <td>Cloudiness: <?php echo $respond->list[0]->weather[0]->description; ?></td>
+        <td>
+            Cloudiness: <?php if (!empty($respond->list[0]->weather[0]->description)) {
+                echo $respond->list[0]->weather[0]->description;
+            } else {
+                echo 'Не удалось получить данные';
+            } ?>
+        </td>
     </tr>
     <tr>
-        <td>Pressure: <?php echo $respond->list[0]->main->pressure;?> hpa</td>
+        <td>
+            Pressure: <?php if (!empty($respond->list[0]->main->pressure)) {
+                echo $respond->list[0]->main->pressure .' hpa';
+            } else {
+                echo 'Не удалось получить данные';
+            } ?>
+        </td>
     </tr>
     <tr>
-        <td>Humidity: <?php echo $respond->list[0]->main->humidity;?>%</td>
+        <td>
+            Humidity: <?php if (!empty($respond->list[0]->main->humidity)) {
+                echo $respond->list[0]->main->humidity . '%';
+            } else {
+                echo 'Не удалось получить данные';
+            } ?>
+        </td>
     </tr>
 </table>
 </body>
